f0d074bbed8bb2a9b87fed52808c2a389585ed81
Josemlg993 <94807261+Josemlg993@users.noreply.github.com>
2025-09-16 19:16:16 +0200

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,17 @@
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open('ugp-cache-v1').then(cache =>
+      cache.addAll([
+        '/',
+        '/index.html',
+        '/manifest.json',
+        '/icono.png'
+      ])
+    )
+  );
+});
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
